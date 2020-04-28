c7ca310b5fb6ef793a0566ba82f957e8ab58230a
Tawhid Ali <tawhidali126@gmail.com>
2020-04-28 20:05:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//  create a function that merges two sorted arrays, and returns a new array sorted
+
+// GAMEPLAN
+ // have a pointer to compare the first element of arr1 
+  // have another pointer to compare the first element of arr2
+
+  // if first element of arr1 < first element of arr2 --- push that element to return arr
+
+  // pointer of arr1 to move up a element while still comparing to first element of arr2
+  // only move pointer for each if element is removed
+
+
+function merge(arr1, arr2) {
+  let pter1 = 0
+  let pter2 = 0
+  let returnArr = []
+
+  while(pter1 < arr1.length && pter2 < arr2.length){
+    if(arr1[pter1] < arr2[pter2]){
+      returnArr.push(arr1[pter1])
+      pter1 ++
+    } else {
+      returnArr.push(arr2[pter2])
+      pter2 ++
+    }
+  }
+
+  while(pter1 < arr1.length){
+    returnArr.push(arr1[pter1])
+    pter1++
+  }
+
+  while(pter2 < arr2.length){
+    returnArr.push(arr2[pter2])
+    pter2++
+  }
+ 
+  return returnArr
+
+
+ 
+}
+
+merge([1,3,5], [2,4,6,8,10])
